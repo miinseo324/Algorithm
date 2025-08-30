705a18b77539fad9fa3bfe6b9d12c89e657b312a
MINSEO PARK <miinseo0324@gmail.com>
2025-08-30 22:50:05 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1192 ms, Memory: 54036 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        StringTokenizer st;
+        // 입출입 기록의 개수 n
+        int n = Integer.parseInt(br.readLine().trim());
+        HashSet<String> set = new HashSet<>();
+        for (int i=0; i<n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            String name = st.nextToken();
+            String log = st.nextToken();
+
+            // enter일 경우
+            if (log.equals("enter") && !set.contains(name)) {
+                set.add(name);
+            } else if (log.equals("leave") && set.contains(name)){ // leave일 경우
+                set.remove(name);
+            }
+        }
+        // set을 List로 변환 후 collections.sort() / collections.reverseOrder() 사용해서 정렬
+        List<String> result = new ArrayList<String>(set);
+        Collections.sort(result, Collections.reverseOrder());
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
